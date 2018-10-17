84f839e9c7a6ad6e8717cfa8115173eea503287f
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-17 21:57:57 +0900

    format

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_multi_track_signal_exporter.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_multi_track_signal_exporter.mm
@@ -7,12 +7,13 @@
 using namespace yas::multi_track;
 
 struct signal_exporter::impl : base::impl {
-    double const _sample_rate;
-    audio::pcm_format const _pcm_format;
+    audio::format _format;
     std::string const _root_path;
 
     impl(double const sample_rate, audio::pcm_format const pcm_format, std::string const &root_path)
-        : _sample_rate(sample_rate), _pcm_format(pcm_format), _root_path(root_path) {
+        : _format(audio::format::args{
+              .sample_rate = sample_rate, .channel_count = 1, .pcm_format = pcm_format, .interleaved = false}),
+          _root_path(root_path) {
     }
 };
 

2cd22fb8d7a5ee6cd79c5627e1b8805bbdd27348
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-18 23:21:05 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_file_manager.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_file_manager.mm
@@ -10,10 +10,9 @@ using namespace yas;
 
 file_manager::create_dir_result_t file_manager::create_directory_if_not_exists(std::string const &path) {
     auto file_manager = [NSFileManager defaultManager];
-    BOOL is_directory = NO;
     CFStringRef cf_path = to_cf_object(path);
 
-    if (![file_manager fileExistsAtPath:(__bridge NSString *)cf_path isDirectory:&is_directory]) {
+    if (auto exists_result = file_manager::file_exists(path); !exists_result) {
         NSError *error = nil;
         if (![file_manager createDirectoryAtPath:(__bridge NSString *)cf_path
                      withIntermediateDirectories:YES
@@ -21,7 +20,7 @@ file_manager::create_dir_result_t file_manager::create_directory_if_not_exists(s
                                            error:&error]) {
             return create_dir_result_t{create_dir_error::create_failed};
         }
-    } else if (!is_directory) {
+    } else if (exists_result.value() == file_kind::file) {
         return create_dir_result_t{create_dir_error::file_exists};
     }
 

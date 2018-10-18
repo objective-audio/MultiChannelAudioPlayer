ea8f6c8ba818fb27aea0f637de4c1cbac8a18b05
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-18 22:35:30 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_multi_track_signal_exporter.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_multi_track_signal_exporter.mm
@@ -9,7 +9,26 @@
 using namespace yas::multi_track;
 
 namespace yas::multi_track::utils {
-#warning ディレクトリがなければ作る関数を追加
+void create_directory_if_not_exists(std::string const &path) {
+    auto file_manager = [NSFileManager defaultManager];
+    BOOL is_directory = NO;
+    CFStringRef cf_path = to_cf_object(path);
+    auto file_exists = [file_manager fileExistsAtPath:(__bridge NSString *)cf_path isDirectory:&is_directory];
+    if (!file_exists) {
+        NSError *error = nil;
+        if (![file_manager createDirectoryAtPath:(__bridge NSString *)cf_path
+                     withIntermediateDirectories:YES
+                                      attributes:nil
+                                           error:&error]) {
+            std::string const message =
+                "create directory failed. '" + to_string((__bridge CFStringRef)error.description) + "'";
+            throw std::runtime_error(message);
+        }
+    } else if (!is_directory) {
+        std::string const message = "path is file. '" + path + "'";
+        throw std::runtime_error(message);
+    }
+}
 }
 
 struct signal_exporter::impl : base::impl {
@@ -21,15 +40,7 @@ struct signal_exporter::impl : base::impl {
         : _format(audio::format::args{
               .sample_rate = sample_rate, .channel_count = 1, .pcm_format = pcm_format, .interleaved = false}),
           _root_path(root_path) {
-        auto file_manager = [NSFileManager defaultManager];
-        BOOL is_directory = NO;
-        CFStringRef cf_path = to_cf_object(this->_root_path);
-        auto file_exists = [file_manager fileExistsAtPath:(__bridge NSString *)cf_path isDirectory:&is_directory];
-        if (!file_exists) {
-#warning directoryを作成
-        } else if (!is_directory) {
-            throw std::runtime_error("root_path is file.");
-        }
+        utils::create_directory_if_not_exists(this->_root_path);
     }
 
     void export_file(uint32_t const trk_idx, proc::time::range const &range,

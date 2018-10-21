b34f5b7442195fd37cce255abc514265cacb2345
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-21 10:34:28 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_math.cpp
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_math.cpp
@@ -10,11 +10,11 @@ using namespace yas;
 int64_t math::floor_int(int64_t const value, uint64_t const interval) {
     if (value == 0) {
         return 0;
-    } else if (int64_t const mod = value % interval; mod == 0) {
+    } else if (int64_t const mod = value % static_cast<int64_t>(interval); mod == 0) {
         return value;
     } else if (value > 0) {
         return value - mod;
     } else {
-        return value - (mod + interval);
+        return value - mod - static_cast<int64_t>(interval);
     }
 }

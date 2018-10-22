da5f3da68a2bd6f4c034b65debd8939595fe94df
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-22 09:46:26 +0900

    no message

---
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_multi_track_audio_exporter_tests.mm
@@ -0,0 +1,23 @@
+//
+//  yas_multi_track_audio_exporter_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_multi_track_audio_exporter.h"
+
+@interface yas_multi_track_audio_exporter_tests : XCTestCase
+
+@end
+
+@implementation yas_multi_track_audio_exporter_tests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+- (void)test_export_file {
+}
+
+@end

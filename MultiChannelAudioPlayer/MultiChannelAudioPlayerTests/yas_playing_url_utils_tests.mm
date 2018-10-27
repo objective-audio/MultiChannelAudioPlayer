f679f430897160115e01d3855d98270c49d91c18
Yuki Yasoshima <yukiyasos@gmail.com>
2018-10-27 11:33:34 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_url_utils_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_url_utils_tests.mm
@@ -22,23 +22,23 @@ using namespace yas::playing;
 - (void)tearDown {
 }
 
-- (void)test_track_url {
+- (void)test_channel_url {
     auto root_url = url::file_url("/root");
     auto track_url = url_utils::channel_url(root_url, 1);
 
     XCTAssertEqual(track_url.path(), "/root/1");
 }
 
-- (void)test_file_url {
-    auto track_url = url_utils::channel_url(yas::url::file_url("/root"), 1);
+- (void)test_caf_url {
+    auto ch_url = url_utils::channel_url(yas::url::file_url("/root"), 1);
 
-    XCTAssertEqual(url_utils::caf_url(track_url, 0, 10).path(), "/root/1/0.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, 1, 10).path(), "/root/1/0.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, 9, 10).path(), "/root/1/0.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, 10, 10).path(), "/root/1/1.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, -1, 10).path(), "/root/1/-1.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, -10, 10).path(), "/root/1/-1.caf");
-    XCTAssertEqual(url_utils::caf_url(track_url, -11, 10).path(), "/root/1/-2.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, 0, 10).path(), "/root/1/0.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, 1, 10).path(), "/root/1/0.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, 9, 10).path(), "/root/1/0.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, 10, 10).path(), "/root/1/1.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, -1, 10).path(), "/root/1/-1.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, -10, 10).path(), "/root/1/-1.caf");
+    XCTAssertEqual(url_utils::caf_url(ch_url, -11, 10).path(), "/root/1/-2.caf");
 }
 
 @end

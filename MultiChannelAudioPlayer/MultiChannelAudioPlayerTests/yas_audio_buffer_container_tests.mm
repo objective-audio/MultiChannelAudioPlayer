84cf33c73e9a35eb980a640ec7d8c8a20d2f8c15
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-02 23:06:07 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_buffer_container_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_buffer_container_tests.mm
@@ -5,6 +5,9 @@
 #import <XCTest/XCTest.h>
 #import "yas_playing_audio_buffer_container.h"
 
+using namespace yas;
+using namespace yas::playing;
+
 @interface yas_audio_buffer_container_tests : XCTestCase
 
 @end
@@ -15,6 +18,20 @@
 }
 
 - (void)tearDown {
+}
+
+- (void)test_initial {
+    uint32_t const file_length = 10;
+    audio::format format{audio::format::args{
+        .sample_rate = file_length, .channel_count = 1, .pcm_format = audio::pcm_format::int16, .interleaved = false}};
+    audio::pcm_buffer buffer{format, file_length};
+    auto const container = make_audio_buffer_container(std::move(buffer));
+
+    XCTAssertTrue(container);
+    XCTAssertFalse(container->file_idx());
+    XCTAssertFalse(container->begin_frame());
+    XCTAssertEqual(container->format(), format);
+    XCTAssertFalse(container->contains(0));
 }
 
 @end

975f394ac102c91355af1d6971d7685a2b6d44d5
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-04 20:10:17 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_system_url_utils.h
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_system_url_utils.h
@@ -9,8 +9,32 @@
 
 namespace yas::system_url_utils {
 enum class dir {
+    application,
+    demo_application,
+    admin_application,
+    library,
+    developer,
+    user,
+    documentation,
     document,
+    core_service,
+    autosaved_information,
+    desktop,
     caches,
+    application_support,
+    downloads,
+    input_methods,
+    movies,
+    music,
+    pictures,
+    printer_description,
+    shared_public,
+    preference_panes,
+    application_scripts,
+    item_replacement,
+    all_applications,
+    all_libraries,
+
     temporary,
 };
 
--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_system_url_utils.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_system_url_utils.mm
@@ -9,15 +9,55 @@
 
 using namespace yas;
 
+/*
+ application,
+ demo_application,
+ admin_application,
+ library,
+ developer,
+ user,
+ documentation,
+ document,
+ core_service,
+ autosaved_information,
+ desktop,
+ caches,
+ application_support,
+ downloads,
+ input_methods,
+ movies,
+ music,
+ pictures,
+ printer_description,
+ shared_public,
+ preference_panes,
+ application_scripts,
+ item_replacement,
+ all_applications,
+ all_libraries,
+ */
 namespace yas {
 static NSSearchPathDirectory to_search_path_directory(system_url_utils::dir const dir) {
     switch (dir) {
+        case system_url_utils::dir::application:
+        case system_url_utils::dir::demo_application:
+        case system_url_utils::dir::admin_application:
+        case system_url_utils::dir::library:
+        case system_url_utils::dir::developer:
+        case system_url_utils::dir::user:
+        case system_url_utils::dir::documentation:
         case system_url_utils::dir::document:
             return NSDocumentDirectory;
+        case system_url_utils::dir::core_service:
+        case system_url_utils::dir::autosaved_information:
+        case system_url_utils::dir::desktop:
         case system_url_utils::dir::caches:
             return NSCachesDirectory;
-        default:
-            throw std::invalid_argument("invalid directory.");
+        case system_url_utils::dir::application_support:
+        case system_url_utils::dir::downloads:
+            case system_url_utils::dir::
+//        default:
+//            throw std::invalid_argument("invalid directory.");
     }
 }
 }

8ba440ee0be649040d7b98d1c6f7974e67d8508f
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-08 09:35:46 +0900

    no message

---
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_circuler_buffer_tests.mm
@@ -0,0 +1,19 @@
+//
+//  yas_audio_circuler_buffer_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yas_audio_circuler_buffer_tests : XCTestCase
+
+@end
+
+@implementation yas_audio_circuler_buffer_tests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+@end

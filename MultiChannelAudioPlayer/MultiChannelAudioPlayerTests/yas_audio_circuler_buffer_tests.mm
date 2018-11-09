ed3ef4e7b1e45f3572af3ece08acdc69be5734c8
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-09 21:19:36 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_circuler_buffer_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_circuler_buffer_tests.mm
@@ -3,6 +3,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "yas_audio_format.h"
+#import "yas_playing_audio_circular_buffer.h"
+
+using namespace yas;
+using namespace yas::playing;
 
 @interface yas_audio_circuler_buffer_tests : XCTestCase
 
@@ -14,6 +19,13 @@
 }
 
 - (void)tearDown {
+}
+
+- (void)test_make {
+    operation_queue queue;
+
+    audio::format format{audio::format::args{
+        .sample_rate = 48000.0, .channel_count = 1, .pcm_format = audio::pcm_format::int16, .interleaved = false}};
 }
 
 @end

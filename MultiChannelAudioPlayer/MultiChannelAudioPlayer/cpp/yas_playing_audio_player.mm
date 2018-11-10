6bcf41a5586e6471f94ba37fc5abf52383d922f0
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-10 13:51:11 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player.mm
@@ -4,6 +4,7 @@
 
 #include "yas_playing_audio_player.h"
 #include "yas_fast_each.h"
+#include "yas_math.h"
 #include "yas_playing_audio_circular_buffer.h"
 #include "yas_playing_url_utils.h"
 
@@ -12,12 +13,14 @@ using namespace yas::playing;
 
 struct audio_player::impl : base::impl {
     audio::format const _format;
+    uint32_t const _file_length;
     url const _root_url;
 
     impl(double const sample_rate, audio::pcm_format const pcm_format, url const &root_url, uint32_t const ch_count)
         : _format(audio::format::args{
               .sample_rate = sample_rate, .channel_count = 1, .pcm_format = pcm_format, .interleaved = false}),
-          _root_url(root_url) {
+          _root_url(root_url),
+          _file_length(static_cast<uint32_t>(sample_rate)) {
         auto each = make_fast_each(ch_count);
         while (yas_each_next(each)) {
             auto const ch_url = url_utils::channel_url(this->_root_url, yas_each_index(each));
@@ -37,6 +40,12 @@ struct audio_player::impl : base::impl {
         std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
         this->_play_frame = play_frame;
+
+        int64_t const top_file_idx = math::floor_int(play_frame, this->_file_length) / this->_file_length;
+
+        for (auto &buffer : this->_buffers) {
+            buffer.reload_all(top_file_idx);
+        }
 #warning todo バッファのリロード
     }
 

7d9c9a62bbfac157904b8d47b15ab7756f54e855
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-11 11:41:18 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_renderer.cpp
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_renderer.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "yas_playing_audio_renderer.h"
+
+using namespace yas;
+using namespace yas::playing;
+
+struct audio_renderer::impl : base::impl {};
+
+audio_renderer::audio_renderer() : base(std::make_shared<impl>()) {
+}
+
+audio_renderer::audio_renderer(std::nullptr_t) : base(nullptr) {
+}
--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_renderer.h
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_renderer.h
@@ -3,3 +3,14 @@
 //
 
 #pragma once
+
+#include "yas_base.h"
+
+namespace yas::playing {
+struct audio_renderer : base {
+    class impl;
+
+    audio_renderer();
+    audio_renderer(std::nullptr_t);
+};
+}  // namespace yas::playing

902735c08597aef3b283633812ad9617c9b7e06f
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-12 08:59:29 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player.mm
@@ -56,19 +56,21 @@ struct audio_player::impl : base::impl {
     chaining::holder<std::optional<audio::format>> _format{nullptr};
     chaining::observer_pool _pool;
 
-    // 以下、要ロック
+    operation_queue _queue;
+
+    // ロックここから
     int64_t _play_frame = 0;
     bool _is_playing = false;
-    operation_queue _queue;
     std::vector<audio_circular_buffer::ptr> _circular_buffers;
-
+    // ロックここまで
     std::recursive_mutex _mutex;
 
     void _setup_chaining() {
         auto weak_player = to_weak(cast<audio_player>());
 
         this->_pool +=
-            this->_sample_rate.chain().combine(this->_pcm_format.chain())
+            this->_sample_rate.chain()
+                .combine(this->_pcm_format.chain())
                 .to([](std::pair<double, audio::pcm_format> const &pair) {
                     if (pair.first > 0.0) {
                         return std::make_optional(audio::format{audio::format::args{.sample_rate = pair.first,
@@ -81,13 +83,11 @@ struct audio_player::impl : base::impl {
                 .receive(this->_format.receiver())
                 .sync();
 
-#warning todo formatとch_countをcombineしてバッファを更新する
-        auto ch_count_flow = this->_ch_count.chain().to_null();
-
         this->_pool += this->_format.chain()
-                           .perform([weak_player](auto const &) {
+                           .combine(this->_ch_count.chain())
+                           .perform([weak_player](auto const &pair) {
                                if (auto player = weak_player.lock()) {
-                                   return player.impl_ptr<impl>()->_update_circular_buffers();
+                                   player.impl_ptr<impl>()->_update_circular_buffers(pair);
                                }
                            })
                            .sync();
@@ -114,17 +114,16 @@ struct audio_player::impl : base::impl {
         }
     }
 
-    void _update_circular_buffers() {
-        if (this->_format.value()) {
-            audio::format const &format = *this->_format.value();
-            uint32_t const ch_count = this->_ch_count.value();
+    void _update_circular_buffers(std::pair<std::optional<audio::format>, uint32_t> const &pair) {
+        auto const &[format, ch_count] = pair;
 
+        if (format && ch_count > 0) {
             std::lock_guard<std::recursive_mutex> lock(this->_mutex);
 
             auto each = make_fast_each(ch_count);
             while (yas_each_next(each)) {
                 auto const ch_url = url_utils::channel_url(this->_root_url, yas_each_index(each));
-                auto buffer = make_audio_circular_buffer(format, 3, ch_url, this->_queue);
+                auto buffer = make_audio_circular_buffer(*format, 3, ch_url, this->_queue);
                 this->_circular_buffers.push_back(std::move(buffer));
             }
         } else {

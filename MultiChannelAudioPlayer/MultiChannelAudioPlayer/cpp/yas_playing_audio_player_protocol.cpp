a27a5fde14520037d84bff8cc9eb790623dea3bc
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-11 11:03:31 +0900

    no message

---
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player_protocol.cpp
@@ -0,0 +1,18 @@
+//
+//  yas_playing_audio_player_protocol.cpp
+//
+
+#include "yas_playing_audio_player_protocol.h"
+
+using namespace yas;
+using namespace yas::playing;
+
+audio_renderable::audio_renderable(std::shared_ptr<impl> impl) : protocol(std::move(impl)) {
+}
+
+audio_renderable::audio_renderable(std::nullptr_t) : protocol(nullptr) {
+}
+
+void audio_renderable::set_rendering_handler(std::function<void(void)> handler) {
+    impl_ptr<impl>()->set_rendering_handler(std::move(handler));
+}
--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player_protocol.h
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/cpp/yas_playing_audio_player_protocol.h
@@ -3,3 +3,19 @@
 //
 
 #pragma once
+
+#include <functional>
+#include "yas_protocol.h"
+
+namespace yas::playing {
+struct audio_renderable : protocol {
+    struct impl : protocol::impl {
+        virtual void set_rendering_handler(std::function<void(void)> &&);
+    };
+
+    explicit audio_renderable(std::shared_ptr<impl> impl);
+    audio_renderable(std::nullptr_t);
+
+    void set_rendering_handler(std::function<void(void)>);
+};
+}  // namespace yas::playing

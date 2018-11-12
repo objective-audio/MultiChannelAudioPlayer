461f52b3d65b955c5444241a272306392dddb362
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-12 22:36:00 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_audio_renderer.cpp
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_audio_renderer.cpp
@@ -12,6 +12,7 @@ struct test_audio_renderer::impl : base::impl, audio_renderable::impl {
     chaining::holder<double> _sample_rate{0.0};
     chaining::holder<audio::pcm_format> _pcm_format{audio::pcm_format::float32};
     chaining::holder<uint32_t> _channel_count{uint32_t(0)};
+    bool _is_rendering = false;
     audio_renderable::rendering_f _rendering_handler;
 
     void set_rendering_handler(audio_renderable::rendering_f &&handler) override {
@@ -37,12 +38,6 @@ test_audio_renderer::test_audio_renderer() : base(std::make_shared<impl>()) {
 test_audio_renderer::test_audio_renderer(std::nullptr_t) : base(nullptr) {
 }
 
-void test_audio_renderer::render(audio::pcm_buffer &buffer) {
-    if (auto handler = impl_ptr<impl>()->_rendering_handler) {
-        handler(buffer);
-    }
-}
-
 void test_audio_renderer::set_pcm_format(audio::pcm_format const pcm_fomat) {
     impl_ptr<impl>()->_pcm_format.set_value(pcm_fomat);
 }
@@ -53,6 +48,16 @@ void test_audio_renderer::set_channel_count(uint32_t const ch_count) {
 
 void test_audio_renderer::set_sample_rate(double const sample_rate) {
     impl_ptr<impl>()->_sample_rate.set_value(sample_rate);
+}
+
+void test_audio_renderer::set_is_rendering(bool const is_rendering) {
+    impl_ptr<impl>()->_is_rendering = is_rendering;
+}
+
+void test_audio_renderer::render(audio::pcm_buffer &buffer) {
+    if (auto handler = impl_ptr<impl>()->_rendering_handler) {
+        handler(buffer);
+    }
 }
 
 audio_renderable &test_audio_renderer::renderable() {
--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_audio_renderer.h
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_audio_renderer.h
@@ -15,10 +15,12 @@ struct test_audio_renderer : base {
     test_audio_renderer();
     test_audio_renderer(std::nullptr_t);
 
-    void render(audio::pcm_buffer &buffer);
     void set_pcm_format(audio::pcm_format const);
     void set_channel_count(uint32_t const);
     void set_sample_rate(double const);
+    void set_is_rendering(bool const);
+
+    void render(audio::pcm_buffer &buffer);
 
     audio_renderable &renderable();
 

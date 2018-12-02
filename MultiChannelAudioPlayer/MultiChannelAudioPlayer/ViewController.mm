cb4ea1a302e0a6f9f4c871dfd7c4613c5b56dfdc
Yuki Yasoshima <yukiyasos@gmail.com>
2018-12-02 10:45:54 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayer/ViewController.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayer/ViewController.mm
@@ -4,16 +4,30 @@
 
 #import "ViewController.h"
 #import "yas_playing_audio.h"
+#import "yas_system_url_utils.h"
+
+using namespace yas;
+using namespace yas::playing;
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+namespace yas::playing::sample {
+struct view_controller_cpp {
+    url root_url{system_url_utils::directory_url(system_url_utils::dir::document).appending("sample")};
+    operation_queue queue;
+    audio_renderer renderer;
+    audio_player player{this->renderer.renderable(), this->root_url, this->queue};
+};
+}
+
+@implementation ViewController {
+    sample::view_controller_cpp _cpp;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 @end

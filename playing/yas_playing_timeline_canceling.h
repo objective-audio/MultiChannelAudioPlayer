733fbd0bf7b03efbc1d70a3c21810d3d3f22a523
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-13 23:05:03 +0900

    no message

--- playing/yas_playing_timeline_canceling.h
+++ playing/yas_playing_timeline_canceling.h
@@ -9,30 +9,30 @@
 #include <processing/yas_processing_umbrella.h>
 
 namespace yas::playing {
-struct timeline_cancel_id : base {
+struct timeline_cancel_matcher_id : base {
     class impl;
 
-    timeline_cancel_id(proc::track_index_t const trk_idx, proc::time::range const &range);
-    timeline_cancel_id(std::nullptr_t);
+    timeline_cancel_matcher_id(proc::track_index_t const trk_idx, proc::time::range const &range);
+    timeline_cancel_matcher_id(std::nullptr_t);
 };
 
 struct timeline_cancel_request : protocol {
     struct impl : protocol::impl {
-        virtual bool is_match(timeline_cancel_id::impl const &) const = 0;
+        virtual bool is_match(timeline_cancel_matcher_id::impl const &) const = 0;
     };
 };
 
-struct timeline_track_cancel_request : base {
+struct timeline_track_cancel_request_id : base {
     class impl;
 
-    timeline_track_cancel_request(proc::track_index_t const trk_idx);
-    timeline_track_cancel_request(std::nullptr_t);
+    timeline_track_cancel_request_id(proc::track_index_t const trk_idx);
+    timeline_track_cancel_request_id(std::nullptr_t);
 };
 
-struct timeline_range_cancel_request : base {
+struct timeline_range_cancel_request_id : base {
     class impl;
 
-    timeline_range_cancel_request(proc::time::range const &range);
-    timeline_range_cancel_request(std::nullptr_t);
+    timeline_range_cancel_request_id(proc::time::range const &range);
+    timeline_range_cancel_request_id(std::nullptr_t);
 };
 }  // namespace yas::playing

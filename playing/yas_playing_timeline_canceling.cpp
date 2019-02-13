f8b34d46065ddca5e237bbdd365a678241daab40
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-13 23:02:30 +0900

    no message

--- playing/yas_playing_timeline_canceling.cpp
+++ playing/yas_playing_timeline_canceling.cpp
@@ -4,31 +4,84 @@
 
 #include "yas_playing_timeline_canceling.h"
 
-#warning todo
-
 using namespace yas;
 using namespace yas::playing;
 
-struct timeline_canceling_matcher::impl : base::impl {
+#pragma mark - canceling_matcher
+
+struct timeline_cancel_id::impl : base::impl {
     proc::track_index_t const trk_idx;
     proc::time::range const range;
 
     impl(proc::track_index_t const trk_idx, proc::time::range const &range) : trk_idx(trk_idx), range(range) {
     }
+
+    bool is_equal(std::shared_ptr<base::impl> const &rhs) const override {
+        if (auto casted_rhs = std::dynamic_pointer_cast<timeline_cancel_request::impl>(rhs)) {
+            return casted_rhs->is_match(*this);
+        }
+        return false;
+    }
 };
 
-timeline_canceling_matcher::timeline_canceling_matcher(proc::track_index_t const trk_idx,
+timeline_cancel_id::timeline_cancel_id(proc::track_index_t const trk_idx,
                                                        proc::time::range const &range)
     : base(std::make_shared<impl>(trk_idx, range)) {
 }
 
-timeline_canceling_matcher::timeline_canceling_matcher(std::nullptr_t) : base(nullptr) {
+timeline_cancel_id::timeline_cancel_id(std::nullptr_t) : base(nullptr) {
 }
 
-struct timeline_canceling_request::impl : base::impl {};
+#pragma mark - track_cancel_request
 
-timeline_canceling_request::timeline_canceling_request() : base(std::make_shared<impl>()) {
+struct timeline_track_cancel_request::impl : base::impl, timeline_cancel_request::impl {
+    proc::track_index_t const trk_idx;
+
+    impl(proc::track_index_t const trk_idx) : trk_idx(trk_idx) {
+    }
+
+    bool is_match(timeline_cancel_id::impl const &matcher_impl) const override {
+        return matcher_impl.trk_idx == this->trk_idx;
+    }
+
+    bool is_equal(std::shared_ptr<base::impl> const &rhs) const override {
+        if (auto casted_rhs = std::dynamic_pointer_cast<timeline_cancel_id::impl>(rhs)) {
+            return this->is_match(*casted_rhs);
+        }
+        return false;
+    }
+};
+
+timeline_track_cancel_request::timeline_track_cancel_request(proc::track_index_t const trk_idx)
+    : base(std::make_shared<impl>(trk_idx)) {
 }
 
-timeline_canceling_request::timeline_canceling_request(std::nullptr_t) : base(nullptr) {
+timeline_track_cancel_request::timeline_track_cancel_request(std::nullptr_t) : base(nullptr) {
+}
+
+#pragma mark - range_cancel_request
+
+struct timeline_range_cancel_request::impl : base::impl, timeline_cancel_request::impl {
+    proc::time::range const range;
+
+    impl(proc::time::range const &range) : range(range) {
+    }
+
+    bool is_match(timeline_cancel_id::impl const &matcher_impl) const override {
+        return matcher_impl.range.is_contain(this->range);
+    }
+
+    bool is_equal(std::shared_ptr<base::impl> const &rhs) const override {
+        if (auto casted_rhs = std::dynamic_pointer_cast<timeline_cancel_id::impl>(rhs)) {
+            return this->is_match(*casted_rhs);
+        }
+        return false;
+    }
+};
+
+timeline_range_cancel_request::timeline_range_cancel_request(proc::time::range const &range)
+    : base(std::make_shared<impl>(range)) {
+}
+
+timeline_range_cancel_request::timeline_range_cancel_request(std::nullptr_t) : base(nullptr) {
 }
--- playing/yas_playing_timeline_canceling.h
+++ playing/yas_playing_timeline_canceling.h
@@ -5,20 +5,34 @@
 #pragma once
 
 #include <cpp_utils/yas_base.h>
+#include <cpp_utils/yas_protocol.h>
 #include <processing/yas_processing_umbrella.h>
 
 namespace yas::playing {
-struct timeline_canceling_matcher : base {
+struct timeline_cancel_id : base {
     class impl;
 
-    timeline_canceling_matcher(proc::track_index_t const trk_idx, proc::time::range const &range);
-    timeline_canceling_matcher(std::nullptr_t);
+    timeline_cancel_id(proc::track_index_t const trk_idx, proc::time::range const &range);
+    timeline_cancel_id(std::nullptr_t);
 };
 
-struct timeline_canceling_request : base {
+struct timeline_cancel_request : protocol {
+    struct impl : protocol::impl {
+        virtual bool is_match(timeline_cancel_id::impl const &) const = 0;
+    };
+};
+
+struct timeline_track_cancel_request : base {
     class impl;
 
-    timeline_canceling_request();
-    timeline_canceling_request(std::nullptr_t);
+    timeline_track_cancel_request(proc::track_index_t const trk_idx);
+    timeline_track_cancel_request(std::nullptr_t);
+};
+
+struct timeline_range_cancel_request : base {
+    class impl;
+
+    timeline_range_cancel_request(proc::time::range const &range);
+    timeline_range_cancel_request(std::nullptr_t);
 };
 }  // namespace yas::playing

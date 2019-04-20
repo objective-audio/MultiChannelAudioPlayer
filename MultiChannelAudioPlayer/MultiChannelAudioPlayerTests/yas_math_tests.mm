b1c58f63eea2690f04e1eae3ca89a44c039e6e05
Yuki Yasoshima <yukiyasos@gmail.com>
2019-04-20 23:40:07 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_math_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_math_tests.mm
@@ -32,4 +32,16 @@ using namespace yas::playing;
     XCTAssertEqual(math::floor_int(-11, 10), -20);
 }
 
+- (void)test_ceil_int {
+    XCTAssertEqual(math::ceil_int(0, 10), 0);
+    XCTAssertEqual(math::ceil_int(1, 10), 10);
+    XCTAssertEqual(math::ceil_int(9, 10), 10);
+    XCTAssertEqual(math::ceil_int(10, 10), 10);
+    XCTAssertEqual(math::ceil_int(11, 10), 20);
+    XCTAssertEqual(math::ceil_int(-1, 10), 0);
+    XCTAssertEqual(math::ceil_int(-9, 10), 0);
+    XCTAssertEqual(math::ceil_int(-10, 10), -10);
+    XCTAssertEqual(math::ceil_int(-11, 10), -10);
+}
+
 @end

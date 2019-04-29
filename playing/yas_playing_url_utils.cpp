d21d41807a48708992ad9f6765b45d10b207d1bd
Yuki Yasoshima <yukiyasos@gmail.com>
2019-04-29 19:06:30 +0900

    no message

--- playing/yas_playing_url_utils.cpp
+++ playing/yas_playing_url_utils.cpp
@@ -11,12 +11,39 @@ std::string url_utils::channel_name(int64_t const ch_idx) {
     return std::to_string(ch_idx);
 }
 
-std::string url_utils::fragment_name(int64_t const frag_idx) {
-    return std::to_string(frag_idx);
+std::string url_utils::fragment_name(int64_t const frg_idx) {
+    return std::to_string(frg_idx);
 }
 
-std::string url_utils::signal_file_name(proc::time::range const &range) {
-    return "signal_" + std::to_string(range.frame) + "_" + std::to_string(range.length) + "_";
+std::string url_utils::sample_type_name(std::type_info const &type_info) {
+    if (type_info == typeid(Float64)) {
+        return "f64";
+    } else if (type_info == typeid(Float32)) {
+        return "f32";
+    } else if (type_info == typeid(int64_t)) {
+        return "i64";
+    } else if (type_info == typeid(uint64_t)) {
+        return "u64";
+    } else if (type_info == typeid(int32_t)) {
+        return "i32";
+    } else if (type_info == typeid(uint32_t)) {
+        return "u32";
+    } else if (type_info == typeid(int16_t)) {
+        return "i16";
+    } else if (type_info == typeid(uint16_t)) {
+        return "u16";
+    } else if (type_info == typeid(int8_t)) {
+        return "i8";
+    } else if (type_info == typeid(uint8_t)) {
+        return "u8";
+    } else {
+        return "";
+    }
+}
+
+std::string url_utils::signal_file_name(proc::time::range const &range, std::type_info const &type_info) {
+    return std::to_string(range.frame) + "_" + std::to_string(range.length) + "_" + sample_type_name(type_info) +
+           ".signal";
 }
 
 yas::url url_utils::channel_url(yas::url const &root_url, int64_t const ch_idx) {
@@ -24,12 +51,12 @@ yas::url url_utils::channel_url(yas::url const &root_url, int64_t const ch_idx) 
 }
 
 yas::url url_utils::fragment_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx) {
-    return url_utils::channel_url(root_url, ch_idx).appending(fragment_name(frg_idx));
+    return channel_url(root_url, ch_idx).appending(fragment_name(frg_idx));
 }
 
-yas::url url_utils::signal_file_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx,
-                                    proc::time::range const &range) {
-    return url_utils::fragment_url(root_url, ch_idx, frg_idx).appending(signal_file_name(range));
+yas::url url_utils::file_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx,
+                             proc::time::range const &range, std::type_info const &type_info) {
+    return fragment_url(root_url, ch_idx, frg_idx).appending(signal_file_name(range, type_info));
 }
 
 yas::url url_utils::caf_url(yas::url const &ch_url, int64_t const file_idx) {
--- playing/yas_playing_url_utils.h
+++ playing/yas_playing_url_utils.h
@@ -10,13 +10,14 @@
 
 namespace yas::playing::url_utils {
 [[nodiscard]] std::string channel_name(int64_t const ch_idx);
-[[nodiscard]] std::string fragment_name(int64_t const frag_idx);
-[[nodiscard]] std::string signal_file_name(proc::time::range const &);
+[[nodiscard]] std::string fragment_name(int64_t const frg_idx);
+[[nodiscard]] std::string sample_type_name(std::type_info const &);
+[[nodiscard]] std::string signal_file_name(proc::time::range const &, std::type_info const &);
 
 [[nodiscard]] yas::url channel_url(yas::url const &root_url, int64_t const ch_idx);
 [[nodiscard]] yas::url fragment_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx);
-[[nodiscard]] yas::url signal_file_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx,
-                                       proc::time::range const &);
+[[nodiscard]] yas::url file_url(yas::url const &root_url, int64_t const ch_idx, int64_t const frg_idx,
+                                proc::time::range const &, std::type_info const &);
 [[nodiscard]] yas::url caf_url(yas::url const &ch_url, int64_t const file_idx);
 [[nodiscard]] int64_t caf_idx(int64_t const file_frame_idx, uint64_t const file_length);
 }  // namespace yas::playing::url_utils

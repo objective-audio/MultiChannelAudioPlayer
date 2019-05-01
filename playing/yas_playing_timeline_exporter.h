5632357c8558c8ceae50af69d9c312e7c40274d9
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-01 21:33:54 +0900

    no message

--- playing/yas_playing_timeline_exporter.h
+++ playing/yas_playing_timeline_exporter.h
@@ -13,6 +13,21 @@ namespace yas::playing {
 struct timeline_exporter : base {
     class impl;
 
+    enum event_type {
+        reset,
+        export_began,
+        export_ended,
+    };
+
+    enum error_type {
+        unknown,
+    };
+
+    struct event {
+        event_type const type;
+        std::optional<proc::time::range> range;
+    };
+
     timeline_exporter(url const &root_url, operation_queue, proc::sample_rate_t const);
     timeline_exporter(std::nullptr_t);
 

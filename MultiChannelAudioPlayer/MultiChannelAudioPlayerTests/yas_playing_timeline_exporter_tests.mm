cf1248ef1379b9291af0c953e363f96e3dcf3a90
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-01 12:48:50 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_timeline_exporter_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_timeline_exporter_tests.mm
@@ -129,6 +129,7 @@ struct yas_playing_timeline_exporter_test_cpp {
         XCTAssertEqual(stream.gcount(), 0);
         XCTAssertTrue(stream.eof());
     }
+
     values[0] = values[1] = 0;
 
     {
@@ -152,6 +153,99 @@ struct yas_playing_timeline_exporter_test_cpp {
 }
 
 - (void)test_set_sample_rate {
+    url &root_url = self->_cpp.root_url;
+    operation_queue &queue = self->_cpp.queue;
+    proc::sample_rate_t const pre_sample_rate = 2;
+    proc::sample_rate_t const post_sample_rate = 3;
+
+    timeline_exporter exporter{root_url, queue, pre_sample_rate};
+
+    queue.wait_until_all_operations_are_finished();
+
+    auto module0 = proc::make_signal_module<int64_t>(10);
+    module0.connect_output(proc::to_connector_index(proc::constant::output::value), 0);
+    auto module1 = proc::make_number_module<int64_t>(11);
+    module1.connect_output(proc::to_connector_index(proc::constant::output::value), 1);
+
+    proc::track track0;
+    track0.push_back_module(module0, {-2, 5});
+    proc::track track1;
+    track1.push_back_module(module1, {10, 1});
+
+    proc::timeline timeline{{{0, track0}, {1, track1}}};
+
+    exporter.set_timeline(timeline);
+
+    queue.wait_until_all_operations_are_finished();
+
+    exporter.set_sample_rate(post_sample_rate);
+
+    queue.wait_until_all_operations_are_finished();
+
+    XCTAssertTrue(file_manager::content_exists(root_url.path()));
+
+    XCTAssertFalse(file_manager::content_exists(url_utils::channel_url(root_url, -1).path()));
+    XCTAssertTrue(file_manager::content_exists(url_utils::channel_url(root_url, 0).path()));
+    XCTAssertTrue(file_manager::content_exists(url_utils::channel_url(root_url, 1).path()));
+    XCTAssertFalse(file_manager::content_exists(url_utils::channel_url(root_url, 2).path()));
+
+    XCTAssertFalse(file_manager::content_exists(url_utils::fragment_url(root_url, 0, -2).path()));
+    XCTAssertTrue(file_manager::content_exists(url_utils::fragment_url(root_url, 0, -1).path()));
+    XCTAssertTrue(file_manager::content_exists(url_utils::fragment_url(root_url, 0, 0).path()));
+    XCTAssertFalse(file_manager::content_exists(url_utils::fragment_url(root_url, 0, 1).path()));
+
+    XCTAssertFalse(file_manager::content_exists(url_utils::fragment_url(root_url, 1, 2).path()));
+    XCTAssertTrue(file_manager::content_exists(url_utils::fragment_url(root_url, 1, 3).path()));
+    XCTAssertFalse(file_manager::content_exists(url_utils::fragment_url(root_url, 1, 4).path()));
+
+    XCTAssertTrue(
+        file_manager::content_exists(url_utils::signal_file_url(root_url, 0, -1, {-2, 2}, typeid(int64_t)).path()));
+    XCTAssertTrue(
+        file_manager::content_exists(url_utils::signal_file_url(root_url, 0, 0, {0, 3}, typeid(int64_t)).path()));
+
+    XCTAssertTrue(url_utils::number_file_url(root_url, 1, 3));
+
+    int64_t values[3];
+
+    values[0] = values[1] = values[2] = 0;
+
+    {
+        auto stream = std::ifstream{url_utils::signal_file_url(root_url, 0, -1, {-2, 2}, typeid(int64_t)).path(),
+                                    std::ios_base::in | std::ios_base::binary};
+        XCTAssertFalse(stream.fail());
+        stream.read((char *)values, sizeof(values));
+        XCTAssertEqual(stream.gcount(), sizeof(int64_t) * 2);
+        XCTAssertEqual(values[0], 10);
+        XCTAssertEqual(values[1], 10);
+        XCTAssertEqual(values[2], 0);
+        stream.read((char *)values, sizeof(values));
+        XCTAssertEqual(stream.gcount(), 0);
+        XCTAssertTrue(stream.eof());
+    }
+
+    values[0] = values[1] = values[2] = 0;
+
+    {
+        auto stream = std::ifstream{url_utils::signal_file_url(root_url, 0, 0, {0, 3}, typeid(int64_t)).path(),
+                                    std::ios_base::in | std::ios_base::binary};
+        XCTAssertFalse(stream.fail());
+        stream.read((char *)values, sizeof(values));
+        XCTAssertEqual(stream.gcount(), sizeof(values));
+        XCTAssertEqual(values[0], 10);
+        XCTAssertEqual(values[1], 10);
+        XCTAssertEqual(values[2], 10);
+        stream.read((char *)values, sizeof(values));
+        XCTAssertEqual(stream.gcount(), 0);
+        XCTAssertTrue(stream.eof());
+    }
+
+    values[0] = values[1] = values[2] = 0;
+
+    {
+        auto stream = std::ifstream{url_utils::number_file_url(root_url, 1, 3).path()};
+        std::string str((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+        XCTAssertEqual(str, "10,11,");
+    }
 }
 
 @end

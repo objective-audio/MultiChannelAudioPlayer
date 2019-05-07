e3a85d321579432d304d8e6dc4de7801b71c8937
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-07 09:35:57 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_utils_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_utils_tests.mm
@@ -6,6 +6,9 @@
 #import <playing/playing.h>
 #import "yas_playing_test_utils.h"
 
+using namespace yas;
+using namespace yas::playing;
+
 @interface yas_playing_test_utils_tests : XCTestCase
 
 @end
@@ -16,6 +19,20 @@
 }
 
 - (void)tearDown {
+}
+
+- (void)test_test_timeline {
+    auto timeline = test_utils::test_timeline(0, 2);
+
+    auto expectation = [self expectationWithDescription:@""];
+    expectation.expectedFulfillmentCount = 5;
+
+    timeline.process({-5, 25}, proc::sync_source{5, 5},
+                     [&expectation](proc::time::range const &range, proc::stream const &stream, bool &stop) {
+                         [expectation fulfill];
+                     });
+
+    [self waitForExpectations:@[expectation] timeout:10.0];
 }
 
 @end

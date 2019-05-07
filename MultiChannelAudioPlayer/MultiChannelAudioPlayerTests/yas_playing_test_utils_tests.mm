bd2bd67e58e914c85c0446f9f20170716aef471d
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-07 09:05:52 +0900

    no message

---
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_playing_test_utils_tests.mm
@@ -0,0 +1,21 @@
+//
+//  yas_playing_test_utils_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <playing/playing.h>
+#import "yas_playing_test_utils.h"
+
+@interface yas_playing_test_utils_tests : XCTestCase
+
+@end
+
+@implementation yas_playing_test_utils_tests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+@end

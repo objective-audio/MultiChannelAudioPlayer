16f482cdfbb187fdae96e935c8cbe976f3e6d678
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-10 22:15:01 +0900

    no message

--- playing/yas_playing_timeline_utils.cpp
+++ playing/yas_playing_timeline_utils.cpp
@@ -5,8 +5,8 @@
 #include "yas_playing_timeline_utils.h"
 #include <audio/yas_audio_format.h>
 #include <cpp_utils/yas_boolean.h>
-#include "yas_playing_math.h"
 #include <fstream>
+#include "yas_playing_math.h"
 
 using namespace yas;
 using namespace yas::playing;
@@ -173,86 +173,123 @@ char const *timeline_utils::char_value_data(proc::number_event const &event) {
     }
 }
 
-std::multimap<frame_index_t, proc::number_event> timeline_utils::read_number_events(std::string const &path) {
+std::optional<std::multimap<frame_index_t, proc::number_event>> timeline_utils::read_number_events(
+    std::string const &path) {
     std::multimap<frame_index_t, proc::number_event> events;
-    
+
     auto stream = std::ifstream{path, std::ios_base::in | std::ios_base::binary};
     if (stream.fail()) {
-        return events;
+        return std::nullopt;
     }
-    
+
     while (!stream.fail() && !stream.eof()) {
         proc::time::frame::type frame;
         stream.read((char *)&frame, sizeof(proc::time::frame::type));
         if (stream.fail() || stream.gcount() != sizeof(proc::time::frame::type)) {
-            return events;
+            return std::nullopt;
         }
-        
+
         sample_store_type store_type;
         stream.read((char *)&store_type, sizeof(sample_store_type));
         if (stream.fail() || stream.gcount() != sizeof(sample_store_type)) {
-            return events;
+            return std::nullopt;
         }
-        
+
         switch (store_type) {
             case sample_store_type::float64: {
                 double value;
                 stream.read((char *)&value, sizeof(double));
-                if (stream.fail()) {
-                    return events;
+                if (stream.fail() || stream.gcount() != sizeof(double)) {
+                    return std::nullopt;
                 }
-                
+                events.emplace(std::move(frame), proc::make_number_event(value));
             } break;
             case sample_store_type::float32: {
                 float value;
                 stream.read((char *)&value, sizeof(float));
-            }break;
+                if (stream.fail() || stream.gcount() != sizeof(float)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
             case sample_store_type::int64: {
                 int64_t value;
                 stream.read((char *)&value, sizeof(int64_t));
-            }break;
+                if (stream.fail() || stream.gcount() != sizeof(int64_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
             case sample_store_type::uint64: {
                 uint64_t value;
                 stream.read((char *)&value, sizeof(uint64_t));
-            }break;
+                if (stream.fail() || stream.gcount() != sizeof(uint64_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
             case sample_store_type::int32: {
                 int32_t value;
                 stream.read((char *)&value, sizeof(int32_t));
-            }break;
+                if (stream.fail() || stream.gcount() != sizeof(int32_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
             case sample_store_type::uint32: {
                 uint32_t value;
                 stream.read((char *)&value, sizeof(uint32_t));
-            }break;
-            case sample_store_type::int16:{
+                if (stream.fail() || stream.gcount() != sizeof(uint32_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
+            case sample_store_type::int16: {
                 int16_t value;
                 stream.read((char *)&value, sizeof(int16_t));
-            }break;
-            case sample_store_type::uint16:{
+                if (stream.fail() || stream.gcount() != sizeof(int16_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
+            case sample_store_type::uint16: {
                 uint16_t value;
                 stream.read((char *)&value, sizeof(uint16_t));
-            }break;
-            case sample_store_type::int8:{
+                if (stream.fail() || stream.gcount() != sizeof(uint16_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
+            case sample_store_type::int8: {
                 int8_t value;
                 stream.read((char *)&value, sizeof(int8_t));
-            }break;
-            case sample_store_type::uint8:{
+                if (stream.fail() || stream.gcount() != sizeof(int8_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
+            case sample_store_type::uint8: {
                 uint8_t value;
                 stream.read((char *)&value, sizeof(uint8_t));
-            }break;
-            case sample_store_type::boolean:{
+                if (stream.fail() || stream.gcount() != sizeof(uint8_t)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(value));
+            } break;
+            case sample_store_type::boolean: {
                 bool value;
                 stream.read((char *)&value, sizeof(bool));
-            }break;
-                
+                if (stream.fail() || stream.gcount() != sizeof(bool)) {
+                    return std::nullopt;
+                }
+                events.emplace(std::move(frame), proc::make_number_event(boolean(value)));
+            } break;
+
             default:
-                return events;
+                return std::nullopt;
         }
-        int64_t value;
-        stream.read((char *)&value, sizeof(int64_t));
-        XCTAssertEqual(stream.gcount(), sizeof(int64_t));
-        XCTAssertEqual(value, 11);
     }
-    
+
     return events;
 }
 

ca033cbe72a8f14b69a6645c853780c1e92971d8
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-15 20:44:22 +0900

    no message

--- playing/yas_playing_signal_file.cpp
+++ playing/yas_playing_signal_file.cpp
@@ -3,9 +3,24 @@
 //
 
 #include "yas_playing_signal_file.h"
+#include <fstream>
+#include "yas_playing_timeline_utils.h"
 
 using namespace yas;
 using namespace yas::playing;
 
-signal_file::signal_file(std::string const &path) : path(path) {
+signal_file::signal_file(std::string const &path) : _path(path) {
+}
+
+void signal_file::write(proc::time::range const &range, proc::signal_event const &event) {
+    std::ofstream stream{this->_path, std::ios_base::out | std::ios_base::binary};
+    if (!stream) {
+        //        return error::open_signal_stream_failed;
+    }
+
+    if (char const *data = timeline_utils::char_data(event)) {
+        stream.write(data, event.byte_size());
+    }
+
+    stream.close();
 }
--- playing/yas_playing_signal_file.h
+++ playing/yas_playing_signal_file.h
@@ -4,13 +4,17 @@
 
 #pragma once
 
+#include <processing/yas_processing_signal_event.h>
+#include <processing/yas_processing_time.h>
 #include <string>
 
 namespace yas::playing {
 struct signal_file {
     signal_file(std::string const &path);
 
+    void write(proc::time::range const &range, proc::signal_event const &event);
+
    private:
-    std::string const path;
+    std::string const _path;
 };
 }  // namespace yas::playing

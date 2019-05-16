7878a6c6da309ff484964c016bbe9aa434cc5f85
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-16 22:44:27 +0900

    no message

--- playing/yas_playing_signal_file.cpp
+++ playing/yas_playing_signal_file.cpp
@@ -4,6 +4,7 @@
 
 #include "yas_playing_signal_file.h"
 #include <audio/yas_audio_format.h>
+#include <audio/yas_audio_types.h>
 #include <fstream>
 #include "yas_playing_timeline_utils.h"
 
@@ -33,25 +34,13 @@ signal_file::write_result_t signal_file::write(std::string const &path, proc::si
     return write_result_t{nullptr};
 }
 
-signal_file::read_result_t signal_file::read(signal_file_info const &info, audio::pcm_buffer &buffer,
-                                             playing::frame_index_t const buf_top_frame) {
-    frame_index_t const buf_next_frame = buf_top_frame + buffer.frame_length();
-
-    if (info.range.frame < buf_top_frame || buf_next_frame < info.range.next_frame()) {
-        return read_result_t{read_error::out_of_range};
-    }
-
-    auto stream = std::fstream{info.path, std::ios_base::in | std::ios_base::binary};
+signal_file::read_result_t signal_file::read(std::string const &path, void *data_ptr, std::size_t const length) {
+    auto stream = std::fstream{path, std::ios_base::in | std::ios_base::binary};
     if (!stream) {
         return read_result_t{read_error::open_stream_failed};
     }
 
-    std::size_t const sample_byte_count = buffer.format().sample_byte_count();
-    frame_index_t const frame = (info.range.frame - buf_top_frame) * sample_byte_count;
-    length_t const length = info.range.length * sample_byte_count;
-    char *data_ptr = timeline_utils::char_data(buffer);
-
-    stream.read(&data_ptr[frame], length);
+    stream.read(reinterpret_cast<char *>(data_ptr), length);
     if (stream.fail()) {
         return read_result_t{read_error::read_from_stream_failed};
     }
@@ -66,4 +55,24 @@ signal_file::read_result_t signal_file::read(signal_file_info const &info, audio
     }
 
     return read_result_t{nullptr};
+}
+
+signal_file::read_result_t signal_file::read(signal_file_info const &info, audio::pcm_buffer &buffer,
+                                             playing::frame_index_t const buf_top_frame) {
+    if (info.sample_type != yas::to_sample_type(buffer.format().pcm_format())) {
+        return read_result_t{read_error::invalid_sample_type};
+    }
+
+    frame_index_t const buf_next_frame = buf_top_frame + buffer.frame_length();
+
+    if (info.range.frame < buf_top_frame || buf_next_frame < info.range.next_frame()) {
+        return read_result_t{read_error::out_of_range};
+    }
+
+    std::size_t const sample_byte_count = buffer.format().sample_byte_count();
+    frame_index_t const frame = (info.range.frame - buf_top_frame) * sample_byte_count;
+    length_t const length = info.range.length * sample_byte_count;
+    char *data_ptr = timeline_utils::char_data(buffer);
+
+    return read(info.path, &data_ptr[frame], length);
 }
--- playing/yas_playing_signal_file.h
+++ playing/yas_playing_signal_file.h
@@ -20,6 +20,7 @@ enum class write_error {
 };
 
 enum class read_error {
+    invalid_sample_type,
     out_of_range,
     open_stream_failed,
     read_from_stream_failed,
@@ -31,5 +32,6 @@ using write_result_t = result<std::nullptr_t, write_error>;
 using read_result_t = result<std::nullptr_t, read_error>;
 
 write_result_t write(std::string const &path, proc::signal_event const &event);
-read_result_t read(signal_file_info const &, audio::pcm_buffer &, playing::frame_index_t const);
+read_result_t read(std::string const &path, void *data_ptr, std::size_t const byte_length);
+read_result_t read(signal_file_info const &, audio::pcm_buffer &, playing::frame_index_t const buf_top_frame);
 }  // namespace yas::playing::signal_file

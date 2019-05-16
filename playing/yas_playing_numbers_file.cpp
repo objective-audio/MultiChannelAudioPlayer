2783a8d43ec8fc41f32a7ec28c30d8900a3d6f76
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-16 10:21:05 +0900

    no message

--- playing/yas_playing_numbers_file.cpp
+++ playing/yas_playing_numbers_file.cpp
@@ -13,7 +13,7 @@ numbers_file::write_result_t numbers_file::write(
     std::string const &path, std::multimap<playing::frame_index_t, proc::number_event> const &events) {
     std::ofstream stream{path, std::ios_base::out | std::ios_base::binary};
     if (!stream) {
-        return write_result_t{error::open_stream_failed};
+        return write_result_t{write_error::open_stream_failed};
     }
 
     for (auto const &event_pair : events) {
@@ -22,7 +22,7 @@ numbers_file::write_result_t numbers_file::write(
         if (char const *data = timeline_utils::char_data(frame)) {
             stream.write(data, sizeof(proc::time::frame::type));
             if (stream.fail()) {
-                return write_result_t{error::write_to_stream_failed};
+                return write_result_t{write_error::write_to_stream_failed};
             }
         }
 
@@ -32,19 +32,38 @@ numbers_file::write_result_t numbers_file::write(
         if (char const *data = timeline_utils::char_data(store_type)) {
             stream.write(data, sizeof(sample_store_type));
             if (stream.fail()) {
-                return write_result_t{error::write_to_stream_failed};
+                return write_result_t{write_error::write_to_stream_failed};
             }
         }
 
         if (char const *data = timeline_utils::char_data(event)) {
             stream.write(data, event.sample_byte_count());
             if (stream.fail()) {
-                return write_result_t{error::write_to_stream_failed};
+                return write_result_t{write_error::write_to_stream_failed};
             }
         }
     }
 
     stream.close();
+    if (stream.fail()) {
+        return write_result_t{write_error::close_stream_failed};
+    }
 
     return write_result_t{nullptr};
+}
+
+numbers_file::read_result_t numbers_file::read(std::string const &path) {
+    std::ofstream stream{path, std::ios_base::in | std::ios_base::binary};
+    if (!stream) {
+        return read_result_t{read_error::open_stream_failed};
+    }
+
+#warning todo
+
+    stream.close();
+    if (stream.fail()) {
+        return read_result_t{read_error::close_stream_failed};
+    }
+
+    return read_result_t{nullptr};
 }
--- playing/yas_playing_numbers_file.h
+++ playing/yas_playing_numbers_file.h
@@ -10,13 +10,21 @@
 #include "yas_playing_types.h"
 
 namespace yas::playing::numbers_file {
-enum class error {
+enum class write_error {
     open_stream_failed,
     write_to_stream_failed,
     close_stream_failed,
 };
 
-using write_result_t = result<std::nullptr_t, error>;
+enum class read_error {
+    open_stream_failed,
+    read_from_stream_failed,
+    close_stream_failed,
+};
+
+using write_result_t = result<std::nullptr_t, write_error>;
+using read_result_t = result<std::nullptr_t, read_error>;
 
 write_result_t write(std::string const &, std::multimap<playing::frame_index_t, proc::number_event> const &);
+read_result_t read(std::string const &);
 }  // namespace yas::playing::numbers_file

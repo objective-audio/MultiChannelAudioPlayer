3c2f1819a4c3145663b22e2992a28ac18dba1ded
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-20 10:13:45 +0900

    no message

--- MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_circular_buffer_chain_tests.mm
+++ MultiChannelAudioPlayer/MultiChannelAudioPlayerTests/yas_audio_circular_buffer_chain_tests.mm
@@ -5,6 +5,7 @@
 #import <XCTest/XCTest.h>
 #import <cpp_utils/cpp_utils.h>
 #import <playing/yas_playing_audio_circular_buffer.h>
+#import "yas_playing_test_utils.h"
 
 using namespace yas;
 using namespace yas::playing;
@@ -27,15 +28,39 @@ struct cpp {
 
 @end
 
-@implementation yas_audio_circular_buffer_chain_tests
+@implementation yas_audio_circular_buffer_chain_tests {
+    audio_circular_buffer_chain_test::cpp _cpp;
+}
 
 - (void)setUp {
+    test_utils::remove_all_document_files();
+
+    self->_cpp.queue = task_queue{queue_priority_count};
 }
 
 - (void)tearDown {
+    self->_cpp.queue = nullptr;
+
+    test_utils::remove_all_document_files();
 }
 
 - (void)test_states_chain {
+    auto &cpp = self->_cpp;
+    task_queue &queue = cpp.queue;
+
+    auto circular_buffer =
+        make_audio_circular_buffer(cpp.format, 2, queue, [](audio::pcm_buffer &buffer, int64_t const frag_idx) {
+            int64_t const top_frame_idx = frag_idx * 3;
+            int16_t *data_ptr = buffer.data_ptr_at_index<int16_t>(0);
+            data_ptr[0] = top_frame_idx;
+            data_ptr[1] = top_frame_idx + 1;
+            data_ptr[2] = top_frame_idx + 2;
+            return true;
+        });
+
+    circular_buffer->reload_all(-1);
+    queue.wait_until_all_tasks_are_finished();
+#warning todo
 }
 
 @end
